fb9d2ea1c9e6714faa478ac4ae3685a5db6b2ff3
Guilherme Rosa <guiprosa@gmail.com>
2024-05-07 10:46:00 -0300

    adição de localstorage

--- js/script.js
+++ js/script.js
@@ -1,17 +1,49 @@
+// Mostra os números e operadores clicados pelo usuário na tela
 function display(valor){
     document.getElementById('resultado').value += valor;
-    return valor
+    return valor;
 }
 
-function solve(){
-    let x = document.getElementById('result').value
-    let y = eval(x);
-    document.getElementById('result').value = y
-    return y
+// Calcula a expressão inserida pelo usuário e retorna o valor para a tela
+function solucao(){
+    let historicoRecuperado = recuperaHistorico();
+
+    let expressao = document.getElementById('resultado').value;
+    let resultado = eval(expressao);
+    document.getElementById('resultado').value = resultado;
+    
+    let objetoResultado = {
+        expressao: expressao,
+        solucao: resultado};
+    salvarHistorico(objetoResultado);
+
+    return resultado;
 }
 
-function clearScreen(){
+// Limpa a tela 
+function limparTela(){
+    document.getElementById('resultado').value = '';
+}
 
-    document.getElementById('result').value = ''
+// Recupera o resultado da solucao()
+function recuperaHistorico() {
+    let historico = localStorage.getItem("historico");
+
+    if(!historico) {
+        return [];
+    }
+
+    let historicoObjeto = JSON.parse(historico);
+
+    return historicoObjeto;
+}
+
+// Salva o resultado como histórico
+function salvarHistorico(resultado) {
+    let historico = recuperaHistorico(); // histórico é um array de obj
+    
+    historico.push(resultado);
+    historico = JSON.stringify(historico);
+    localStorage.setItem("historico", historico);
 
 }
