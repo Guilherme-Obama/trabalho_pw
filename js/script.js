660d765463d1ec8b1a7be371759234878558af0f
Guilherme Rosa <guiprosa@gmail.com>
2024-05-08 10:38:15 -0300

    funções novas js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
-document.getElementById("resultado").focus()
+document.getElementById("resultado").focus(); // Retorna o usuário para o campo de input
+const campoInput = document.getElementById("resultado"); // Constante de validação do input
+const regex = /^([-+]?\d*\.?\d+)(?:\s*([-+*\/])\s*([-+]?\d*\.?\d+))*$/ // Expressão regex para validação do input
 
-//verificar letras e operadores regex=[-+*\/]|\d+(\.\d+)?\.? regex.test(string)
-// Mostra os números e operadores clicados pelo usuário na tela
+// Evento para quando o usuário pressionar tecla Enter, Esc e "c"
+campoInput.addEventListener("keydown", function(event){
+    if (event.key === "Enter") {
+        solucao();
+    } else if (event.key === "Escape" || event.key === "c") {
+        limparTela();
+    }
+})
+
+// Filtra o input para operadores e números válidos
+function filtroInput(event){
+    const validacaoValor = event.target.value;
+    const valorFiltrado = validacaoValor.replace(/[^-+*/\d.]/g, ''); // Substitui inputs inválidos por vazio 
+    event.target.value = valorFiltrado;
+}
+
+campoInput.addEventListener('input', filtroInput); // Executa o filtro de input
+
+// Mostra os números e operadores clicados pelo usuário na tela da calculadora
 function display(valor){
-    document.getElementById('resultado').value += valor;
+    document.getElementById("resultado").value += valor;
     document.getElementById("resultado").focus();
     return valor;
 }
@@ -12,14 +31,25 @@ function display(valor){
 function solucao(){
     let historicoRecuperado = recuperaHistorico();
 
-    let expressao = document.getElementById('resultado').value;
-    let resultado = eval(expressao);
-    document.getElementById('resultado').value = resultado;
-    
-    let objetoResultado = {
-        expressao: expressao,
-        solucao: resultado};
-    salvarHistorico(objetoResultado);
+    let expressao = document.getElementById("resultado").value;
+    try {
+        let resultado = eval(expressao);
+        if (resultado == Infinity) {
+            alert("Erro! Divisão por zero!");
+            document.getElementById("resultado").value = '';
+            return;
+        }
+        document.getElementById("resultado").value = resultado.toFixed(2);
+        
+        let objetoResultado = {
+            expressao: expressao,
+            solucao: resultado.toFixed(2)};
+        salvarHistorico(objetoResultado);
+        
+    } catch (error) {
+        document.getElementById("resultado").value = '';
+        alert("Expressão: " + expressao + " inválida!")
+    }
     
     document.getElementById("resultado").focus();
 
@@ -28,7 +58,7 @@ function solucao(){
 
 // Limpa a tela 
 function limparTela(){
-    document.getElementById('resultado').value = '';
+    document.getElementById("resultado").value = '';
     document.getElementById("resultado").focus();
 }
 
